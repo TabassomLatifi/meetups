d642696a3388dde60a355cb6e9af286f0f4521d8
Tabassom <tabassom.latifi@gmail.com>
2023-11-07 11:13:42 +0330

    fixed fallback

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2eae7135e661ae59"; }
+/******/ 		__webpack_require__.h = function() { return "6c7b7f1ae9b3fd85"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
